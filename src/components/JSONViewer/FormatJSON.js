d008c4714741da0a100c90559fb84019c4bb4582
Markus <markholden1989@googlemail.com>
2020-04-01 16:12:17 +0100

    Removed redundant code

--- src/components/JSONViewer/FormatJSON.js
+++ src/components/JSONViewer/FormatJSON.js
@@ -5,10 +5,6 @@ import Auxillary from "./../../hoc/Auxillary";
 
 const FormatJSON = (props) => {
     function buildFormattedJSON(data) {
-        let items = [];
-        // debugger;
-        // Is current obj an Array or Object?
-
         if (Array.isArray(data)) {
             return (
                 <div className={style["formatted-json__array"]}>
@@ -86,125 +82,7 @@ const FormatJSON = (props) => {
                     })}
                 </div>
             );
-        } else {
         }
-
-        // console.log(data);
-
-        // let keys = Object.keys(data);
-
-        // let i = 0,
-        //     l = keys.length;
-
-        // for (i; i < l; i++) {
-        //     let currentKey = keys[i],
-        //         currentValue = data[currentKey];
-
-        //     // * is array?
-        //     if (Array.isArray(currentValue)) {
-        //         items.push(
-        //             <div
-        //                 className={style["formatted-json__array"]}
-        //                 key={currentKey + "__array"}
-        //             >
-        //                 <span className={style["formatted-json__entry__key"]}>
-        //                     {currentKey}
-        //                 </span>
-        //                 <span className={style["formatted-json__entry__entry"]}>
-        //                     {buildFormattedJSON(currentValue)}
-        //                 </span>
-        //             </div>
-        //         );
-        //         continue;
-        //     }
-
-        //     // * is object
-        //     if (typeof currentValue === "object" && currentValue !== null) {
-        //         items.push(
-        //             // <div
-        //             //     className={style["formatted-json__object"]}
-        //             //     key={currentKey + "__object"}
-        //             // >
-        //             //     <span className={style["formatted-json__entry__key"]}>
-        //             //         {currentKey}
-        //             //     </span>
-        //             //     <span className={style["formatted-json__entry__entry"]}>
-        //             //         {buildFormattedJSON(currentValue)}
-        //             //     </span>
-        //             // </div>
-
-        //             <div
-        //                 className={style["formatted-json__entry"]}
-        //                 key={currentKey + "__object"}
-        //             >
-        //                 <span className={style["formatted-json__entry__key"]}>
-        //                     {currentKey}
-        //                 </span>
-        //                 {" : "}
-        //                 <div className={style["formatted-json__object"]}>
-        //                     {buildFormattedJSON(currentValue)}
-        //                 </div>
-        //             </div>
-        //         );
-        //         continue;
-        //     }
-
-        //     let valueHTML;
-
-        //     // * test value HTML
-        //     if (
-        //         typeof currentValue === "string" &&
-        //         /(jpg|jpeg|png|gif|svg{1,})/.test(
-        //             currentValue.trim().toLowerCase()
-        //         )
-        //     ) {
-        //         valueHTML = (
-        //             <Auxillary>
-        //                 <span>{currentValue}</span>
-        //                 <div
-        //                     className={
-        //                         style[
-        //                             "formatted-json__entry__value__img-wrapper"
-        //                         ]
-        //                     }
-        //                 >
-        //                     <img src={currentValue} alt="" />
-        //                 </div>
-        //             </Auxillary>
-        //         );
-        //     } else if (
-        //         typeof currentValue === "string" &&
-        //         /(http{1,})/.test(currentValue.trim().toLowerCase())
-        //     ) {
-        //         valueHTML = (
-        //             <span className={style["formatted-json__entry__value"]}>
-        //                 <a href={currentValue}>{currentValue}</a>
-        //             </span>
-        //         );
-        //     } else {
-        //         valueHTML = (
-        //             <span className={style["formatted-json__entry__value"]}>
-        //                 {JSON.stringify(currentValue)}
-        //             </span>
-        //         );
-        //     }
-
-        //     // * Is just key and value
-        //     items.push(
-        //         <div
-        //             className={style["formatted-json__entry"]}
-        //             key={currentKey + "__" + currentValue}
-        //         >
-        //             <span className={style["formatted-json__entry__key"]}>
-        //                 {currentKey}
-        //             </span>
-        //             {" : "}
-        //             {valueHTML}
-        //         </div>
-        //     );
-        // }
-
-        return items;
     }
 
     return (
